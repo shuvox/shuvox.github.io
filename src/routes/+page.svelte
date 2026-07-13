945b6d2a93ad851508ff933354009b814a49c5e1
ShadmanSShuvo <shadmansshuvo@gmail.com>
2026-07-13 16:18:27 +0600

    remove teaching & mail

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -92,19 +92,7 @@
             <div
               class="flex flex-col sm:flex-row justify-center lg:justify-start items-center space-y-2 sm:space-y-0 sm:space-x-6 text-sm text-ink_light/70 dark:text-ink_dark/70"
             >
-              <div
-                class="flex items-center space-x-2 hover:text-secondary_light dark:hover:text-secondary_dark transition-colors duration-300"
-              >
-                <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
-                  <path
-                    d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"
-                  />
-                  <path
-                    d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"
-                  />
-                </svg>
-                <span><a href="mailto:sdmnsvo@gmail.com">sdmnsvo@gmail.com</a></span>
-              </div>
+              
               <div
                 class="flex items-center space-x-2 hover:text-secondary_light dark:hover:text-secondary_dark transition-colors duration-300"
               >
